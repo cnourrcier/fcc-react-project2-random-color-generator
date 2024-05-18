29233d91f3996250066cea9a829eccfc0997409c
Charlie Nourrcier <cnourrcier@hotmail.com>
2024-05-18 14:20:59 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Random Color Generator
+
+Part 2 of the Build 25 React Projects challenge. This is a random color generator which displays a random color in either hex or rgb.
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import RandomColor from './components/randomColor';
+
+function App() {
+
+  return (
+    <>
+      <RandomColor />
+    </>
+  )
+}
+
+export default App
